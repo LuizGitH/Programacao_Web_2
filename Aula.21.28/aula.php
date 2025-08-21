55b77fd7da43dfded202619390f1ae58a8c4c8a1
Luiz Felipe Evangelista Assunção <20241050100002@ifg.br>
2025-08-21 20:33:19 -0300

    Atualizações em ANOTACOES e aula.php

--- Aula.21.28/aula.php
+++ Aula.21.28/aula.php
@@ -7,8 +7,51 @@
 </head>
 <body>
     <?php
-        echo "<h1> Primeira pagina em php <h1></h1>";
-        $variavel = "";
+        echo "<h1> Primeira pagina em php </h1>";
+        $variavel = "IFG";
+
+        echo "<h1> Eu estudo no $variavel</h1>";
+
+        $n1= 5;
+        $n2= 5;
+        $soma = $n1 + $n2;
+
+        echo "<h1> Minha faculdade é $soma<h1>";
+
+        if ($soma ==0){
+            echo "<h1> Meu primeiro if </h1>";
+        } elseif ($soma ==7){
+            echo "<h1> Meu primeiro elseif </h1>";
+        } else {
+            echo "<h1> Meu primeiro  if, elseif, else </h1>";
+        }
+
+        echo "<h1> ______________ Exercício de Condição ______________ <h1>";
+
+        $idade = 20;
+
+        if ($idade <18){
+            echo "<h1> Menor de idade </h1>";
+        } elseif ($idade == 18 ){
+            echo "<h1> Em transição </h1>";
+        } else {
+            echo "<h1> Maior de idade </h1>";
+        }
+
+        echo "<h1> ______________ Exercício de Repetição ______________ <h1>";
+
+        $x = 0;
+        while ($x < 10){
+        echo "<h4> Valor de x é: $x</h4>";
+        $x++;
+        }
+        echo "<hr>";
+        for ($y=1; $y <= 5 ; $y++){
+            echo "<h4> O valor de y é $y</h4>";
+        }
+
+
+
     ?>
 </body>
 </html>
